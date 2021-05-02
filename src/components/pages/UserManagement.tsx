7d6b96502e9b66109e19e8d93d9dae6c9f0358e9
harauma <fot39hara@gmail.com>
2021-05-02 16:43:54 +0900

    ユーザー詳細モーダル コンポーネント分割

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -2,23 +2,14 @@
 import { memo, useCallback, useEffect, VFC } from "react";
 import {
   Center,
-  FormControl,
-  FormLabel,
-  Input,
-  Modal,
-  ModalBody,
-  ModalCloseButton,
-  ModalContent,
-  ModalHeader,
-  ModalOverlay,
   Spinner,
-  Stack,
   useDisclosure,
   Wrap,
   WrapItem
 } from "@chakra-ui/react";
 
 import { UserCard } from "../organisms/user/UserCard";
+import { UserDetailModal } from "../organisms/user/UserDetailModal";
 import { useAllUsers } from "../../hooks/useAllUsers";
 
 export const UserManagement: VFC = memo(() => {
@@ -50,38 +41,7 @@ export const UserManagement: VFC = memo(() => {
           )
         </Wrap>
       )}
-      <Modal
-        isOpen={isOpen}
-        onClose={onClose}
-        autoFocus={false}
-        motionPreset="slideInBottom"
-      >
-        <ModalOverlay />
-        <ModalContent pb={6}>
-          <ModalHeader>ユーザー詳細</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody mx={4}>
-            <Stack spacing={4}>
-              <FormControl>
-                <FormLabel>名前</FormLabel>
-                <Input value="hoge" isReadOnly />
-              </FormControl>
-              <FormControl>
-                <FormLabel>フルネーム</FormLabel>
-                <Input value="hogehoge" isReadOnly />
-              </FormControl>
-              <FormControl>
-                <FormLabel>MAIL</FormLabel>
-                <Input value="hoge@hoge.com" isReadOnly />
-              </FormControl>
-              <FormControl>
-                <FormLabel>TEL</FormLabel>
-                <Input value="080-1234-5678" isReadOnly />
-              </FormControl>
-            </Stack>
-          </ModalBody>
-        </ModalContent>
-      </Modal>
+      <UserDetailModal isOpen={isOpen} onClose={onClose} />
     </>
   );
 });
